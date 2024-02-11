1376e492bf7f64c41c9a04f920a7fe9af0e15024
Marco Stuurman <marco@fe.ax>
2024-02-11 20:33:13 +0100

    Update backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "tfstate-feaf737c58ed9d44"
+    bucket         = "tfstate-b79910bcf31c5e9d"
     key            = "states/terraform.tfstate"
     encrypt        = true
-    dynamodb_table = "app-state-feaf737c58ed9d44"
+    dynamodb_table = "app-state-b79910bcf31c5e9d"
     region         = "eu-central-1"
   }
 }
